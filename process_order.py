40f18d1d04adb33b1bd94f723186040f3764d217
withjak <akshbishnoi@gmail.com>
2021-10-25 19:14:38 +0530

    refactor and comments

--- process_order.py
+++ process_order.py
@@ -8,23 +8,26 @@ from event import Event
 
 
 def input_reader(filepath):
+    '''Read json input from file
+    '''
     with open(filepath) as f:
-        data = json.load(f)
-    return data
+        records = json.load(f)
+    return records
 
 
 if __name__ == '__main__':
+
+    # user defining what actions to take when a condition is met.
+    condition_action_map = [
+        (condition.Check('location', 'Pune'), [action.send_alert]),
+        (condition.MatchCount('location', 'Banglore', 2), [action.send_alerts, action.print_events])
+    ]
+    
+    # reading input from file
     dir = os.path.dirname(__file__)
     input_records = input_reader(os.path.join(dir, 'input.json'))
 
-
-    condition_action_map = [
-        (condition.Check('location', 'Pune'), [action.send_alert]),
-        (condition.MatchCount('location', 'Banglore', 2), [action.send_alerts, 
-                                                           action.print_events])
-    ]
-    
-
+    # to simulate a stream of records comming into the system
     for record in input_records:
         print('-'*10)
         print('New record: ', record)
