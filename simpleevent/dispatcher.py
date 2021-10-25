d54fbf9f7f4295c54f7f49ed671b4d075aaa97ac
withjak <akshbishnoi@gmail.com>
2021-10-25 19:18:52 +0530

    checking condition type for MultiEventCondition
    will help in case more condition types are added. Or if user fails to inherit from base class .

--- simpleevent/dispatcher.py
+++ simpleevent/dispatcher.py
@@ -1,10 +1,16 @@
-from .condition_base import SingleEventCondition
+from .condition_base import SingleEventCondition, MultiEventCondition
 
 
 # Global variables are bad practice
 # this is just to demonstrate the idea of a dispatcher
 event_log = []
+
 def dispatcher(event, condition_action_map):
+    '''
+    Checks which single event condition evaluates to True for this event.
+    Also checks which multi event conditions evaluates to True considering 
+    all events till now
+    '''
     event_log.append(event)
 
     for condn, actions in condition_action_map:
@@ -12,7 +18,7 @@ def dispatcher(event, condition_action_map):
             if condn.evaluate(event):
                 take_action(event, actions)
         
-        else:
+        elif isinstance(condn, MultiEventCondition):
             matching_events = condn.evaluate(event_log)
             if matching_events:
                 take_action(matching_events, actions)
